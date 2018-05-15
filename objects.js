08f2a019a20d848251571ce5535fb2a1dfb59533
Rayan Dutta <rondutta23@gmail.com>
2018-05-15 20:45:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,10 +8,13 @@ var updatePlaylist = function(playlist,artist,title) {
   });
 }
 
+var play;
+
 var removeFromPlaylist = function(playlist,artist){
-  var check = delete playlist.artist;
-  if(!check)
-   return;
-  else
+  for (play in playlist){
+    if (play===artist){
+      delete playlist.artist;
+    }
+  }
   return playlist;
 }
